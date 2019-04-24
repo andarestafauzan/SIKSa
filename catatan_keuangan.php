c841438aad927e4838696ef443e399e35675a748
JaviarFasyah <fasyahj@yahoo.com>
2019-04-24 22:10:25 +0700

    Update catatan_keuangan.php

--- catatan_keuangan.php
+++ catatan_keuangan.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Catat Penjualan</title>
+	<title>Catatan Keuangan</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
